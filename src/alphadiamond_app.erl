1aea704cb9549124cbfdbc714ea25aba9fd861b3
Andy Marks <amarks@thoughtworks.com>
2016-01-27 21:23:13 +1100

    Prep for command line run

--- src/alphadiamond_app.erl
+++ src/alphadiamond_app.erl
@@ -5,7 +5,7 @@
 -define(ROW_TEMPLATE, "ZYXWVUTSRQPONMLKJIHGFEDCBABCDEFGHIJKLMNOPQRSTUVWXYZ").
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([diamond/1, start/2, stop/1]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -20,20 +20,6 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
-
-type_of(X) when is_integer(X)   -> integer;
-type_of(X) when is_float(X)     -> float;
-type_of(X) when is_list(X)      -> list;
-type_of(X) when is_tuple(X)     -> tuple;
-type_of(X) when is_bitstring(X) -> bitstring;  % will fail before e12
-type_of(X) when is_binary(X)    -> binary;
-type_of(X) when is_boolean(X)   -> boolean;
-type_of(X) when is_function(X)  -> function;
-type_of(X) when is_pid(X)       -> pid;
-type_of(X) when is_port(X)      -> port;
-type_of(X) when is_reference(X) -> reference;
-type_of(X) when is_atom(X)      -> atom;
-type_of(_X)                     -> unknown.
 
 row_instructions_for(Letter) -> 
 	Alphabet = lists:seq($A,$Z),
