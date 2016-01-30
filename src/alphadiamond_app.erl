4d952b48d29c10e7f4c4b8ada3209408d4cce51f
Andy Marks <amarks@thoughtworks.com>
2016-01-30 21:52:23 +1100

    Reformat only

--- src/alphadiamond_app.erl
+++ src/alphadiamond_app.erl
@@ -29,12 +29,19 @@ everything_but(Letter) -> 							"[^" ++ Letter ++ "]".
 row_for(Letter) ->
 	re:replace(?ROW_TEMPLATE, everything_but(Letter), " ", [global, {return,list}]).
 
+is_valid_spec(Spec) when is_atom(Spec) -> is_valid_spec(atom_to_list(Spec)); 
 is_valid_spec(Spec) -> 
+	% io:format(user, "\n~p\n", [Spec]),
 	TrimmedSpec = string:strip(Spec),
 	CleanSpec = re:replace(TrimmedSpec, "[^A-Za-z]", "", [global, {return,list}]),
 	(CleanSpec =:= TrimmedSpec) and (length(CleanSpec) == 1).
 
-diamond([Spec|_]) -> 
+diamond([Spec|_]) -> diamond(Spec);
+diamond([]) -> 
+	io:format(user, "\nINVALID INPUT\n", []),
+	false;
+diamond(Spec) when is_atom(Spec) -> diamond(atom_to_list(Spec));
+diamond(Spec) ->
 	ValidSpec = is_valid_spec(Spec),
     if
     	ValidSpec ->
@@ -52,39 +59,37 @@ diamond() ->
 
 -ifdef(TEST).
 
-row_test_() ->
-	[
-		?_assertEqual("A", string:strip(row_for("A"))),
-		?_assertEqual("B B", string:strip(row_for("B"))),
-		?_assertEqual("J                 J", string:strip(row_for("J")))
-	].
+row_test_() -> [
+	?_assertEqual("A", string:strip(row_for("A"))),
+	?_assertEqual("B B", string:strip(row_for("B"))),
+	?_assertEqual("J                 J", string:strip(row_for("J")))
+].
 
-row_instructions_test_() ->
-	[
-		?_assertEqual("A", row_instructions_for("A")),
-		?_assertEqual("ABA", row_instructions_for("B")),
-		?_assertEqual("ABCBA", row_instructions_for("C"))
-	].
+row_instructions_test_() -> [
+	?_assertEqual("A", row_instructions_for("A")),
+	?_assertEqual("ABA", row_instructions_for("B")),
+	?_assertEqual("ABCBA", row_instructions_for("C"))
+].
 
-valid_spec_test_() ->
-	[
-		?_assert(is_valid_spec("A")),
-		?_assert(is_valid_spec("c")),
-		?_assert(is_valid_spec("  E   ")),
-		?_assertNot(is_valid_spec("AA")),
-		?_assertNot(is_valid_spec(" ")),
-		?_assertNot(is_valid_spec("4")),
-		?_assertNot(is_valid_spec("{")),
-		?_assertNot(is_valid_spec(""))
-	].
+valid_spec_test_() -> [
+	?_assert(is_valid_spec("A")),
+	?_assert(is_valid_spec('A')),
+	?_assert(is_valid_spec("c")),
+	?_assert(is_valid_spec("  E   ")),
+	?_assertNot(is_valid_spec("AA")),
+	?_assertNot(is_valid_spec(" ")),
+	?_assertNot(is_valid_spec("4")),
+	?_assertNot(is_valid_spec("{")),
+	?_assertNot(is_valid_spec(""))
+].
 
-positive_smoke_test() ->
-    ?assert(diamond(['Z'])).
+positive_smoke_test_() ->
+    ?_assert(diamond(['Z'])).
 
-negative_smoke_test_() ->
-	[
-    	?_assertNot(diamond([";"])),
-    	?_assertNot(diamond())
-    ].
+negative_smoke_test_() -> [
+	?_assertNot(diamond([";"])),
+	?_assertNot(diamond()),
+	?_assertNot(diamond([]))
+].
 
 -endif.
